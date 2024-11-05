725b496a52a9c81f2704777f8ac899dcdf5c63d1
Jason Flores <floresjason06@gmail.com>
2024-11-04 19:58:50 -0500

    first-commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...value) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < value.length; j++) {
+      if (array[i] === value[j]) {
+        array.splice(i, 1);
+        i--;
+        break;
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
